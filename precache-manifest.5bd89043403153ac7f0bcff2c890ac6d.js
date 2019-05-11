3954db5cd534c83acb98ce9a01a301ee4b2b4221
Keing <hunganhly2208@gmail.com>
2019-05-12 03:37:58 +0700

    Updates

---
+++ precache-manifest.5bd89043403153ac7f0bcff2c890ac6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832fa8d7dbcc6fa8776ddda20a49d059",
+    "url": "/gitpages/index.html"
+  },
+  {
+    "revision": "da3eaa7fe6d7078103f1",
+    "url": "/gitpages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gitpages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "da3eaa7fe6d7078103f1",
+    "url": "/gitpages/static/js/main.64f0662b.chunk.js"
+  },
+  {
+    "revision": "e445cbcbcfd4fa8ceda0",
+    "url": "/gitpages/static/js/runtime~main.6dd7791e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
